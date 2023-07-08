7a734a8945f6e616d795e88127aa307d9efe0c8f
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-07 22:42:18 -0300

    principal do sistema

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listadeusuarios from "./pages/lista_de_usuarios";
+
 function App() {
-  return <div>OLaaa</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Listadeusuarios />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
