0ebe1c71d1d1f0e2916bebe5a2c5f0673f36a57f
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-09 11:08:59 -0300

    editando usuário

---
+++ src/pages/editar_usuario.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from "../components/navbar";
+
+export default function Editarusuario() {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUsuario();
+  }, []);
+
+  function getUsuario() {
+    axios
+      .get(`http://127.0.0.1:5000/detalhedousuario/${id}`) // Correção: usar crases/graves
+      .then(function (response) {
+        console.log(response.data);
+        setInputs(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .put(`http://127.0.0.1:5000/atualizarusuario/${id}`, inputs)
+      .then(function (response) {
+        console.log(response.data);
+        navigate("/");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div className="container h-100">
+          <div className="row">
+            <div className="col-2"></div>
+            <div className="col-8">
+              <p className="fs-1">Editar usuario</p>
+              <form onSubmit={HandleSubmit}>
+                <div className="mb-3">
+                  <label>Nome</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="nome"
+                    value={inputs.nome}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    value={inputs.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  name="atualizar"
+                  className="btn btn-primary"
+                >
+                  Salvar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
