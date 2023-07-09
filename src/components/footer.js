c1ca232b29d7e28885fa7b7a65286730a3d4a069
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-09 11:41:41 -0300

    novo componente

---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <footer class="bg-dark text-center">
+        <br />
+        <br />
+        <p class="mb-1 text-danger">©Cadastralog Inc 2023</p>
+        <br />
+        <br />
+      </footer>
+    </>
+  );
+}
